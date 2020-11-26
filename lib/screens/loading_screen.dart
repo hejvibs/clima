d5515be0df9f37de151615a58229fe74c7526acb
Vibhakar Sarswat <vibhakar.sarswat@productmadness.com>
2020-11-26 20:00:36 +0000

    Added 'http' package. Made a simple 'get' request using http package and retrieved and displayed data from 'response.body'

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -19,8 +20,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print('Current latitude is: ${location.latitude} and longitude is: ${location.longitude}');
   }
 
+  void getData() async {
+    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?q=london&appid=2df3bbc58358e57b293149d9ca8d8f01');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print('Error code received: => ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
