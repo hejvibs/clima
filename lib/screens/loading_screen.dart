98e9fd1948f976d97e89b073f56daf0d56e556d4
Vibhakar Sarswat <vibhakar.sarswat@productmadness.com>
2020-11-26 14:47:55 +0000

    Added Skeleton code. Method to get Current location using 'Geolocator' package and updated 'AndroidManifest.xml' and 'info.plist' respectively.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,41 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    debugPrint(permission.toString());
+    if (permission == LocationPermission.always || permission == LocationPermission.whileInUse) {
+      printLocation();
+    } else {
+      requestPermission();
+    }
+  }
+
+  requestPermission() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.always || permission == LocationPermission.whileInUse) {
+      printLocation();
+    } else {
+      requestPermission();
+    }
+  }
+
+  printLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.medium);
+      print(position.toString());
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            // Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
